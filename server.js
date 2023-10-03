a6c2868343c96a5199d46c977224f64fcac0b46d
gianluca.rizzo2 <gianluca.rizzo2@sistinf.it>
2023-10-03 14:52:09 +0200

    impl

--- server.js
+++ server.js
@@ -1,53 +1,52 @@
 const express = require('express');
 const { Pool } = require('pg');
 const bodyParser = require('body-parser');
-const axios = require('axios');
-const cors = require('cors'); // Import the cors package
-const port = 3000;
-// Create a new Express application
-const app = express();
+const cors = require('cors');
 
-// Configure the PostgreSQL connection
+const app = express();
+const port = process.env.PORT || 3000;
+
+// PostgreSQL connection options
 const pool = new Pool({
-  user: 'admin',
-  host: 'localhost',
-  database: 'test',
-  password: 'admin',
-  port: 5432 // default PostgreSQL port
+  user: process.env.POSTGRES_USER || 'default',
+  host: process.env.POSTGRES_HOST || 'localhost',
+  database: process.env.POSTGRES_DATABASE || 'verceldb',
+  password: process.env.POSTGRES_PASSWORD || 'E1g7HuMAkcRn',
+  port: process.env.POSTGRES_PORT || 5432,
+  // Add other connection options as needed
+  // Example: ssl: { rejectUnauthorized: false }
 });
 
-// Parse incoming request bodies in a middleware before your handlers
 app.use(bodyParser.json());
 app.use(cors());
 
-// Define a route to handle form submissions
-app.post('/api/form', (req, res) => {
-  const { name, email, people, note } = req.body;
-  console.log(req.body);
-  // Insert the form data into the PostgreSQL database
-  pool.query('INSERT INTO your_table (name, email, people, note) VALUES ($1, $2, $3, $4)', [name, email, people, note], (error, result) => {
-    if (error) {
-      console.error('Error executing query', error);
-      res.status(500).send('Error saving form submission');
-    } else {
-      console.log('Form submission saved successfully');
-      res.status(200).send('Form submission saved');
-    }
-  });
+app.post('/api/form', async (req, res) => {
+  try {
+    const { name, email, people, note } = req.body;
+    console.log(req.body);
+
+    const result = await pool.query(
+      'INSERT INTO your_table (name, email, people, note) VALUES ($1, $2, $3, $4)',
+      [name, email, people, note]
+    );
+
+    console.log('Form submission saved successfully');
+    res.status(200).send('Form submission saved');
+  } catch (error) {
+    console.error('Error executing query', error);
+    res.status(500).send('Error saving form submission');
+  }
 });
 
-// Define a route to test the database connection
-app.get('/api/test-db', (req, res) => {
-  // Execute a simple query to check the database connection
-  pool.query('SELECT 1', (error, result) => {
-    if (error) {
-      console.error('Error executing database query', error);
-      res.status(500).send('Error testing database connection');
-    } else {
-      console.log('Database connection test successful');
-      res.status(200).send('Database connection test successful');
-    }
-  });
+app.get('/api/test-db', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT 1');
+    console.log('Database connection test successful');
+    res.status(200).send('Database connection test successful');
+  } catch (error) {
+    console.error('Error executing database query', error);
+    res.status(500).send('Error testing database connection');
+  }
 });
 
 pool.connect((err, client, release) => {
@@ -59,9 +58,6 @@ pool.connect((err, client, release) => {
   }
 });
 
-// Start the server
-app.listen(process.env.PORT || port, () => {
-  console.log('Server is running on port 3000');
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
-
-// node server.js to start the server
